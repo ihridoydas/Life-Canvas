41884804af73070d814fa291b04d8ec46afd4161
Hridoy Chandra Das <hridoyqualias@gmail.com>
2024-03-26 16:20:30 +0900

    Implement hilt, data and domain layer and lint check

--- network/src/main/java/info/hridoydas/lifecanvas/network/LifeCanvasHttpClientBuilder.kt
+++ network/src/main/java/info/hridoydas/lifecanvas/network/LifeCanvasHttpClientBuilder.kt
@@ -1,5 +1,28 @@
+/*
+* MIT License
+*
+* Copyright (c) 2024 Hridoy Chandra Das
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+*/
 package info.hridoydas.lifecanvas.network
-
 
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
@@ -19,7 +42,6 @@ import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
 class LifeCanvasHttpClientBuilder {
-
     private lateinit var protocol: URLProtocol
     private lateinit var host: String
     private var port: Int? = null
@@ -32,7 +54,6 @@ class LifeCanvasHttpClientBuilder {
 
     fun build(): HttpClient {
         return HttpClient(CIO) {
-
             expectSuccess = true
 
             engine {
@@ -59,11 +80,11 @@ class LifeCanvasHttpClientBuilder {
                         prettyPrint = true
                         isLenient = true
                         ignoreUnknownKeys = true
-                    }
+                    },
                 )
             }
 
-            install(Auth){
+            install(Auth) {
                 bearer {
                     loadTokens {
                         BearerTokens("", "")
@@ -74,15 +95,14 @@ class LifeCanvasHttpClientBuilder {
                 }
             }
 
-            install(Logging){
-                logger = object: Logger {
+            install(Logging) {
+                logger = object : Logger {
                     override fun log(message: String) {
                         println(message)
                     }
                 }
                 level = LogLevel.ALL
             }
-
         }
     }
 }
--- network/src/main/java/info/hridoydas/lifecanvas/network/RequestHandler.kt
+++ network/src/main/java/info/hridoydas/lifecanvas/network/RequestHandler.kt
@@ -1,3 +1,27 @@
+/*
+* MIT License
+*
+* Copyright (c) 2024 Hridoy Chandra Das
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+*/
 package info.hridoydas.lifecanvas.network
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
@@ -9,17 +33,15 @@ import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.appendPathSegments
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
 import java.lang.Exception
 
 class RequestHandler(val httpClient: HttpClient) {
-
     suspend inline fun <reified B, reified R> executeRequest(
         method: HttpMethod,
         urlPathSegments: List<Any>,
         body: B? = null,
-        queryParams: Map<String, Any>? = null
+        queryParams: Map<String, Any>? = null,
     ): NetworkResult<R> {
         return withContext(Dispatchers.IO) {
             try {
@@ -43,7 +65,7 @@ class RequestHandler(val httpClient: HttpClient) {
                     when (e.response.status) {
                         HttpStatusCode.Unauthorized -> NetworkException.UnauthorizedException(
                             errorBody.message,
-                            e
+                            e,
                         )
 
                         else -> NetworkException.NotFoundException("API Not Found", e)
@@ -59,19 +81,20 @@ class RequestHandler(val httpClient: HttpClient) {
     suspend inline fun <reified R> get(
         urlPathSegments: List<Any>,
         queryParams: Map<String, Any>? = null,
-    ): NetworkResult<R> = executeRequest<Any, R>(
-        method = HttpMethod.Get,
-        urlPathSegments = urlPathSegments.toList(),
-        queryParams = queryParams,
-    )
+    ): NetworkResult<R> =
+        executeRequest<Any, R>(
+            method = HttpMethod.Get,
+            urlPathSegments = urlPathSegments.toList(),
+            queryParams = queryParams,
+        )
 
     suspend inline fun <reified B, reified R> post(
         urlPathSegments: List<Any>,
         body: B? = null,
-    ): NetworkResult<R> = executeRequest(
-        method = HttpMethod.Post,
-        urlPathSegments = urlPathSegments.toList(),
-        body = body,
-    )
-
+    ): NetworkResult<R> =
+        executeRequest(
+            method = HttpMethod.Post,
+            urlPathSegments = urlPathSegments.toList(),
+            body = body,
+        )
 }
